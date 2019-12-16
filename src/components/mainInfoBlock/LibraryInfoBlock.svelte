72eb782edbced3fe742f43a5ab0ead9c2d387961
Денис Лежнев <d.lezhnev@teleport.media>
2019-12-16 21:16:14 +0500

    Добавил блок информации в библиотеку

---
+++ src/components/mainInfoBlock/LibraryInfoBlock.svelte
@@ -0,0 +1,61 @@
+<div class="wrap">
+    <div class="container">
+        <div class="block">
+            <div class="container-poster">
+                <div class="logo"></div>
+            </div>
+            <div class="container-text">
+                <div class="text">Представьте, что вы играете в паззл. И как бы не переставляли его части, получается единая картина. Только каждый раз новая.</div>
+                <div class="text">Mixtory — именно такой паззл. Перемешивайте предложения в случайном порядке и читайте каждый раз новую историю.</div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<style>
+    .wrap{
+        background-color: #FFC700;
+        background-image: url("/assets/main_library_text_things.svg");
+    }
+    .container{
+        margin: auto;
+        width: 1400px;
+        padding: 0 120px;
+        box-sizing: border-box;
+    }
+
+    .block{
+        display: flex;
+    }
+
+    .block > .container-poster{
+        width: 480px;
+        height: 250px;
+        background-color: #120D24;
+        background-image:
+            url("/assets/main_library_poster_things.svg"),
+            url("/assets/main_library_poster.svg");
+    }
+
+    .block > .container-poster > .logo{
+        background-image: url("/assets/main_library_poster_logo.svg");
+        width: 100%;
+        height: 100%;
+        background-repeat: no-repeat;
+        background-position: center;
+    }
+
+    .block > .container-text{
+        padding: 64px 0 0 64px;
+        flex: 1;
+    }
+
+    .block > .container-text > .text{
+        font-style: normal;
+        font-weight: 600;
+        font-size: 17px;
+        line-height: 25px;
+        color: #120D24;
+        margin-bottom: 25px;
+    }
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,7 +1,14 @@
+<script>
+	import LibraryInfoBlock from "../components/mainInfoBlock/LibraryInfoBlock.svelte";
+	import StoryList from "../components/storyList/StoryList.svelte";
+</script>
 <style>
 
 </style>
 
 <svelte:head>
-	<title>Mixtory</title>
+	<title>Mixtory - Библиотека</title>
 </svelte:head>
+
+<LibraryInfoBlock/>
+<StoryList/>
